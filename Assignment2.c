6c626441192279b2dca8df60016de413650afa8c
Parmeet7168 <128074833+Parmeet7168@users.noreply.github.com>
2023-03-18 21:47:03 +0530

    Update Assignment2.c

--- Assignment2.c
+++ Assignment2.c
@@ -1,7 +1,32 @@
-//A number is said to be valid if it is divisible by 8. Write a program that allows the user to keep entering numbers as long as the input is valid and also displays a count of the valid numbers. Use a while loop to complete the task
+//Write a C program that allows the user to enter 'N' numbers and find the total positive numbers and total negative numbers using a for loop.
 
-int main() 
-{
+#include<stdio.h>
+
+int main()  
+{  
+    int elements, num, positive = 0, negative = 0,i;  
   
-  return 0;
+    printf("Enter the numbers of elements:");  
+    scanf("%d", &elements);  
+  
+    printf("Enter %d numbers\n", elements);  
+  
+    for(i=0;i<elements;i++)  
+    {  
+        scanf("%d", &num);  
+  
+        if(num >= 0)  
+        {  
+            positive++;  
+        }  
+        else 
+        {  
+            negative++;  
+        }
+    }  
+  
+    printf("\nNumber of Positive values: %d\n", positive);  
+    printf("Number of Negative Values: %d\n", negative);  
+  
+    return 0;                  //name-Parmeet kaur ,Roll no. 2210997168
 }
